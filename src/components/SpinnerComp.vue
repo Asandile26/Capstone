e411bf6ac0fbfabf1c0210c18b493dde64231cb3
Asandile <asandilelangeni@gmail.com>
2023-03-15 16:41:25 +0200

    ala

---
+++ src/components/SpinnerComp.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="spinner">
+        <div class="spinner-border" role="status">
+  <span class="visually-hidden">Loading...</span>
+</div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'SpinnerComp'
+}
+</script>
+<style lang="">
+    
+</style>
